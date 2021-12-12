2374f2cc77b3ba95b9b2e8544059731ba289d261
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-12-12 12:19:05 +0100

    day12 final

--- aoc201/day12/12.py
+++ aoc201/day12/12.py
@@ -18,61 +18,34 @@ for line in f.readlines():
     caves.add(cave_2)
 
 cave_visit_map = {k: 1 for k in caves}
-cave_visit_map_2 = {k: 2 for k in caves}
-cave_visit_map_2["start"] = 1
-cave_visit_map_2["end"] = 1
 
 
-def find_paths(cave, cave_visit_map, path):
-    if cave.islower():
-        cave_visit_map[cave] -= 1
-    path = path + "," + cave
-    if cave == "end":
-        print(path)
-        return 1
-    result = 0
-    for n in cavern_map[cave]:
-        if cave_visit_map[n] > 0:
-            result += find_paths(n, copy.deepcopy(cave_visit_map), path)
-    return result
-
-
-def find_paths_2(cave, cave_visit_map, path, small_single_picked):
+def find_paths(cave, cave_visit_map, small_single_picked):
     if cave.islower():
         if cave_visit_map[cave] > 0:
             cave_visit_map[cave] -= 1
         elif cave not in ["start", "end"]:
             small_single_picked = True
-    path = path + "," + cave
     if cave == "end":
-        #  print(path)
         return 1
     result = 0
     for n in cavern_map[cave]:
-        if cave_visit_map[n] > 0:
-            result += find_paths_2(
+        if cave_visit_map[n] > 0 or (not small_single_picked and n.islower() and n not in ["start", "end"]):
+            result += find_paths(
                 n,
                 copy.deepcopy(cave_visit_map),
-                path,
-                copy.deepcopy(small_single_picked),
-            )
-        elif not small_single_picked and n.islower() and n not in ["start", "end"]:
-            result += find_paths_2(
-                n,
-                copy.deepcopy(cave_visit_map),
-                path,
-                copy.deepcopy(small_single_picked),
+                small_single_picked,
             )
     return result
 
 
 def solution_1():
-    return find_paths("start", copy.deepcopy(cave_visit_map), "")
+    return find_paths("start", copy.deepcopy(cave_visit_map), True)
 
 
 def solution_2():
-    return find_paths_2("start", cave_visit_map, "", False)
+    return find_paths("start", copy.deepcopy(cave_visit_map), False)
 
 
-# print(solution_1())
+print(solution_1())
 print(solution_2())
