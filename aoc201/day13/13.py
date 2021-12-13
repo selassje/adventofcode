4d1ab2a21c22b959eaf5848773d660fd2b9733d2
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-12-13 23:39:54 +0100

    13 day2

--- aoc201/day13/13.py
+++ aoc201/day13/13.py
@@ -25,36 +25,26 @@ def count_dots():
 def fold_left(x):
     global h
     global w
-    folded_count = x
-    new_cols = 0 if (w - x) - x > 0 else abs((w - x) - x)
-    for _ in range(new_cols):
-        for y in range(h):
-            pattern[y].append(False)
-    for x2 in range(folded_count):
+    for x2 in range(x):
         for y in range(h):
             pattern[y][x + x2 + 1] = pattern[y][x + x2 + 1] or pattern[y][x - x2 - 1]
-    for _ in range(folded_count + 1):
+    for _ in range(x + 1):
         for y in range(h):
             pattern[y].pop(0)
-    w -= folded_count + 1
-    w += new_cols
-    pass
+    w -= x + 1
 
 
 def fold_up(y):
     global h
     global w
     folded_count = h - y - 1
-    new_rows = 0 if y - folded_count > 0 else abs(y - folded_count)
-    for _ in range(new_rows):
-        pattern.insert(0, [False for _ in range(w)])
+    assert y == int(h / 2)
     for y2 in range(folded_count):
         for x in range(w):
             pattern[y - y2 - 1][x] = pattern[y - y2 - 1][x] or pattern[y + y2 + 1][x]
     for _ in range(folded_count + 1):
         pattern.pop()
     h -= folded_count + 1
-    h += new_rows
 
 
 class Command(Enum):
@@ -91,7 +81,6 @@ w += 1
 pattern = [[False for _ in range(w)] for _ in range(h)]
 for (x, y) in coordinates:
     pattern[y][x] = True
-# print_pattern()
 for i, cmd in enumerate(commands):
     (c, v) = cmd
     if c == Command.FOLD_LEFT:
@@ -100,3 +89,5 @@ for i, cmd in enumerate(commands):
         fold_up(v)
     if i == 0:
         print(count_dots())
+
+print_pattern()
