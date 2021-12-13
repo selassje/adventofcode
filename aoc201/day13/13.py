316d7fe31bc769da7873a7cebe259fba149ad5d0
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-12-13 23:13:08 +0100

    day13 part1

--- aoc201/day13/13.py
+++ aoc201/day13/13.py
@@ -1,5 +1,4 @@
 from enum import Enum
-import enum
 import re
 
 
@@ -14,6 +13,15 @@ def print_pattern():
     print()
 
 
+def count_dots():
+    result = 0
+    for row in pattern:
+        for dot in row:
+            if dot:
+                result += 1
+    return result
+
+
 def fold_left(x):
     global h
     global w
@@ -24,13 +32,12 @@ def fold_left(x):
             pattern[y].append(False)
     for x2 in range(folded_count):
         for y in range(h):
-            pattern[y][x - x2 + 1] = pattern[y][x - x2 + 1] or pattern[y][x + x2 - 1]
+            pattern[y][x + x2 + 1] = pattern[y][x + x2 + 1] or pattern[y][x - x2 - 1]
     for _ in range(folded_count + 1):
         for y in range(h):
             pattern[y].pop(0)
     w -= folded_count + 1
     w += new_cols
-    print("fold left")
     pass
 
 
@@ -59,20 +66,20 @@ coordinates = []
 commands = []
 h = 0
 w = 0
-f = open("example.txt")
+f = open("input.txt")
 for line in f.readlines():
     if line != "\n":
         match = re.match("fold along y=(\d+)", line)
         if match != None:
             command = Command.FOLD_UP
-            command.FOLD_UP.y = int(match.group(1))
-            commands.append(command)
+            y = int(match.group(1))
+            commands.append((command, y))
             continue
         match = re.match("fold along x=(\d+)", line)
         if match != None:
             command = Command.FOLD_LEFT
-            command.FOLD_LEFT.x = int(match.group(1))
-            commands.append(command)
+            x = int(match.group(1))
+            commands.append((command, x))
             continue
         (x, y) = tuple(map(int, line.split(",")))
         w = max(w, x)
@@ -84,10 +91,12 @@ w += 1
 pattern = [[False for _ in range(w)] for _ in range(h)]
 for (x, y) in coordinates:
     pattern[y][x] = True
-
-for c in commands:
+# print_pattern()
+for i, cmd in enumerate(commands):
+    (c, v) = cmd
     if c == Command.FOLD_LEFT:
-        fold_left(c.FOLD_LEFT.x)
+        fold_left(v)
     else:
-        fold_up(c.FOLD_UP.y)
-    print_pattern()
+        fold_up(v)
+    if i == 0:
+        print(count_dots())
