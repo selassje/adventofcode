e49f959c43c38f9abf83bf08158157419234f6c3
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-12-13 20:45:35 +0100

    dabbling

--- aoc201/day13/13.py
+++ aoc201/day13/13.py
@@ -15,13 +15,39 @@ def print_pattern():
 
 
 def fold_left(x):
+    global h
+    global w
+    folded_count = x
+    new_cols = 0 if (w - x) - x > 0 else abs((w - x) - x)
+    for _ in range(new_cols):
+        for y in range(h):
+            pattern[y].append(False)
+    for x2 in range(folded_count):
+        for y in range(h):
+            pattern[y][x - x2 + 1] = pattern[y][x - x2 + 1] or pattern[y][x + x2 - 1]
+    for _ in range(folded_count + 1):
+        for y in range(h):
+            pattern[y].pop(0)
+    w -= folded_count + 1
+    w += new_cols
     print("fold left")
     pass
 
 
 def fold_up(y):
-    print("fold_up")
-    pass
+    global h
+    global w
+    folded_count = h - y - 1
+    new_rows = 0 if y - folded_count > 0 else abs(y - folded_count)
+    for _ in range(new_rows):
+        pattern.insert(0, [False for _ in range(w)])
+    for y2 in range(folded_count):
+        for x in range(w):
+            pattern[y - y2 - 1][x] = pattern[y - y2 - 1][x] or pattern[y + y2 + 1][x]
+    for _ in range(folded_count + 1):
+        pattern.pop()
+    h -= folded_count + 1
+    h += new_rows
 
 
 class Command(Enum):
@@ -33,20 +59,19 @@ coordinates = []
 commands = []
 h = 0
 w = 0
-
 f = open("example.txt")
 for line in f.readlines():
     if line != "\n":
         match = re.match("fold along y=(\d+)", line)
         if match != None:
             command = Command.FOLD_UP
-            command.FOLD_UP.y = match.group(1)
+            command.FOLD_UP.y = int(match.group(1))
             commands.append(command)
             continue
         match = re.match("fold along x=(\d+)", line)
         if match != None:
             command = Command.FOLD_LEFT
-            command.FOLD_LEFT.x = match.group(1)
+            command.FOLD_LEFT.x = int(match.group(1))
             commands.append(command)
             continue
         (x, y) = tuple(map(int, line.split(",")))
