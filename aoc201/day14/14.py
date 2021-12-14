3a2e0d4bff76ff2ac878bf4fb86fb661b6a5b783
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-12-14 10:17:43 +0100

    day14 part 2

--- aoc201/day14/14.py
+++ aoc201/day14/14.py
@@ -1,4 +1,4 @@
-import sys
+import copy
 
 f = open("input.txt")
 polymer = f.readline().strip()
@@ -22,18 +22,46 @@ def insert():
 
 
 def calc_max_min_diff():
-    count = {}
-    for c in polymer:
-        count[c] = count.get(c, 0) + 1
-    max_count = 0
-    min_count = sys.maxsize
-    for cnt in count.values():
+    max_count = list(count_single.values())[0]
+    min_count = list(count_single.values())[0]
+    for cnt in count_single.values():
         max_count = max(cnt, max_count)
-        min_count = min(cnt, min_count)
+        if cnt > 0:
+            min_count = min(cnt, min_count)
     return max_count - min_count
 
 
-for _ in range(10):
-    polymer = insert()
+count_single = {}
+count_pairs = {}
+for c in polymer:
+    count_single[c] = count_single.get(c, 0) + 1
+for i in range(1, len(polymer)):
+    f = polymer[i - 1]
+    s = polymer[i]
+    count_pairs[f + s] = count_pairs.get(f + s, 0) + 1
+
+for i in range(40):
+    new_count_single = copy.deepcopy(count_single)
+    new_count_pairs = copy.deepcopy(count_pairs)
+    for pair, pair_cnt in count_pairs.items():
+        if pair_cnt > 0 and pair in formulas:
+            inserted = formulas[pair]
+            new_count_single[inserted] = new_count_single.get(inserted, 0) + pair_cnt
+            new_count_pairs[pair[0] + inserted] = (
+                new_count_pairs.get(pair[0] + inserted, 0) + pair_cnt
+            )
+            new_count_pairs[inserted + pair[1]] = (
+                new_count_pairs.get(inserted + pair[1], 0) + pair_cnt
+            )
+            new_count_pairs[pair] -= pair_cnt
+
+    for k in list(new_count_pairs):
+        if new_count_pairs[k] == 0:
+            del new_count_pairs[k]
+    count_single = new_count_single
+    count_pairs = new_count_pairs
+
+    if i == 9:
+        print(calc_max_min_diff())
 
 print(calc_max_min_diff())
