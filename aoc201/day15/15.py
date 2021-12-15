e475281add244568f0b108ef215dab911ea32b26
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-12-15 20:29:05 +0100

    day15 part1

---
+++ aoc201/day15/15.py
@@ -0,0 +1,53 @@
+import sys
+
+cavern = []
+f = open("input.txt")
+for line in f.readlines():
+    line = line.strip()
+    cavern.append(list(map(int, line)))
+
+h = len(cavern)
+w = len(cavern[0])
+
+d = [[sys.maxsize for _ in range(w)] for _ in range(h)]
+d[0][0] = 0
+q = []
+for x in range(w):
+    for y in range(h):
+        q.append((x, y, d[y][x]))
+q.sort(key=lambda x: x[2], reverse=True)
+
+
+def find(q, x, y):
+    for (i, j, _) in q:
+        if i == x and y == j:
+            return True
+    return False
+
+
+def update(q, x, y, newd):
+    for (i, j, d) in q:
+        if i == x and y == j:
+            q.remove((i, j, d))
+            break
+    q.append((x, y, newd))
+
+
+while len(q) > 0:
+    (x, y, _) = q.pop()
+    for i, j in [(-1, 0), (1, 0), (-1, -1), (0, 1)]:
+        ix = x + i
+        jy = y + j
+        if find(
+            q,
+            ix,
+            jy,
+        ):
+            alt = d[y][x] + cavern[jy][ix]
+            if alt < d[jy][ix]:
+                update(q, ix, jy, alt)
+                d[jy][ix] = alt
+
+    q.sort(key=lambda x: x[2], reverse=True)
+
+print(d[-1][-1])
